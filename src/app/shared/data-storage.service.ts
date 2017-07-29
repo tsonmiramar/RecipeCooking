2483213ccc7ff43300ea79afd206b7b611cbe5d5
Son Do <tsonmiramar@gmail.com>
2017-07-28 19:04:05 -0700

    minor change

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
-
+import 'rxjs';
 import { Http, Response } from '@angular/http'
 
 @Injectable()
@@ -22,10 +22,18 @@ export class DataStorageService {
   }
 
   fetchData(){
-    this.http.get('https://recipecooking-fce59.firebaseio.com/recipes.json').subscribe((response: Response)=>{
-      if ( response.status === 200 ){
-        this.recipeService.setRecipes((<Recipe[]>response.json()));
+    this.http.get('https://recipecooking-fce59.firebaseio.com/recipes.json')
+    .map((response:Response)=>{
+      const recipes: Recipe[] = response.json();
+      for ( let recipe of recipes){
+        if (!recipe.ingredients){
+          recipe.ingredients = [];
+        }
       }
+      return recipes;
+    })
+    .subscribe((recipes: Recipe[])=>{
+        this.recipeService.setRecipes(recipes);
     });
   }
 }
